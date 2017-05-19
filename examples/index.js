003d1578f1b3d050ed8929fdea3c087f8ed7ad87
LiangCY <liangcynju@qq.com>
2017-05-19 10:23:04 +0800

    npm publish test

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button} from 'meng';
+
 
 const App = () => (
   <div>
     <h1>Examples</h1>
+    <h2>Button</h2>
+    <Button>Button</Button>
   </div>
 );
 
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,79 @@
 import React, {Component, PropTypes} from 'react';
+import buttonStyle from '../styles/button';
+
+const buttonPropTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  style: PropTypes.object,
+  activeStyle: PropTypes.object,
+  focusStyle: PropTypes.object,
+  hoverStyle: PropTypes.object,
+  disabledStyle: PropTypes.object,
+  disabledHoverStyle: PropTypes.object,
+  onMouseDown: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  primary: PropTypes.bool,
+};
 
 export default class Button extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      isActive: false,
+      isHovered: false
+    };
   }
+
+  static displayName = 'Button';
+
+  static propTypes = buttonPropTypes;
+
+  static defaultProps = {
+    primary: false,
+    disabled: false
+  };
+
+  _onMouseEnter = (event) => {
+    this.setState({
+      isHovered: true
+    });
+
+    if (this.props.onMouseEnter) {
+      this.props.onMouseEnter(event);
+    }
+  };
+
+
+  _onMouseLeave = (event) => {
+    this.setState({
+      isHovered: false,
+    });
+
+    if (this.props.onMouseLeave) {
+      this.props.onMouseLeave(event);
+    }
+  };
+
+  _onMouseDown = (event) => {
+    if (this.props.onMouseDown) {
+      this.props.onMouseDown(event);
+    }
+  };
 
   render() {
     return (
-      <button>
+      <button
+        onMouseEnter={this._onMouseEnter}
+        onMouseLeave={this._onMouseLeave}
+        onMouseDown={this._onMouseDown }
+        disabled={this.props.disabled}>
         {this.props.children}
       </button>
     );
